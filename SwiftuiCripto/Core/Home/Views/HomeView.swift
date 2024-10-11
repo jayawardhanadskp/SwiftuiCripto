106951385c3a5de9e4799123a7356983acd3a52b
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-10-11 22:45:15 +0530

    home view

--- SwiftuiCripto/Core/Home/Views/HomeView.swift
+++ SwiftuiCripto/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -22,6 +23,17 @@ struct HomeView: View {
                 
                 homeHeader
                 
+                columnTitles
+                
+                if !showPortfolio {
+                    allCoinList
+                        .transition(.move(edge: .leading))
+                } else {
+                    portfolioCoinList
+                        .transition(.move(edge: .trailing))
+                }
+               
+                
                 Spacer(minLength: 0)
             }
         }
@@ -33,6 +45,7 @@ struct HomeView: View {
         HomeView()
             .navigationBarHidden(true)
     }
+    .environmentObject(DeveloperPreview.instance.homeVM)
 }
 
 extension HomeView {
@@ -59,6 +72,42 @@ extension HomeView {
                     }
                 }
         }
+        .padding(.horizontal)
+    }
+    
+    private var allCoinList: some View {
+        List {
+            ForEach(vm.allCoin) {coin in
+                CoinRowView(coin: coin, showHoaldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 0 ))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioCoinList: some View {
+        List {
+            ForEach(vm.portfolioCoins) {coin in
+                CoinRowView(coin: coin, showHoaldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 0 ))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var columnTitles:some View {
+        HStack {
+            Text("coin")
+            Spacer()
+            if showPortfolio {
+                Text("Hoaldings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+
+        }
+        .font(.caption)
+        .foregroundColor(Color.theme.secondaryText)
         .padding(.horizontal)
     }
 }
