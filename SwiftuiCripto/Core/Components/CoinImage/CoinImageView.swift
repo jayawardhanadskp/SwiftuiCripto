47e208dc99ae50bc9dd924005def0f4ede46832e
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-10-15 14:12:09 +0530

    coin image view

---
+++ SwiftuiCripto/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,40 @@
+//
+//  CoinImageView.swift
+//  SwiftuiCripto
+//
+//  Created by I O N Groups Pvt Ltd on 2024-10-15.
+//
+
+import SwiftUI
+
+
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+#Preview {
+    CoinImageView(coin: DeveloperPreview.instance.coin)
+        .padding()
+        .previewLayout(.sizeThatFits)
+}
